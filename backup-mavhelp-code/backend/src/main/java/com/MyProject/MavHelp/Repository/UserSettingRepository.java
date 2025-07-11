50cdb5d7ddc9ce8c47257531b1c3fdf7187ac4df
Saugat1017 <saugat1.772419@thc.tu.edu.np>
2025-07-11 18:12:01 -0500

    Unified backend code from recovered and main into single backend folder

--- backup-mavhelp-code/backend/src/main/java/com/MyProject/MavHelp/Repository/UserSettingRepository.java
+++
@@ -1,7 +0,0 @@
-package com.MyProject.MavHelp.Repository;
-
-import com.MyProject.MavHelp.Entity.UserSettings;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserSettingRepository extends JpaRepository<UserSettings, String> {
-}
